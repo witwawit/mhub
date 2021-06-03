9743d07ca20b15be44dd0fc207fdaa320054e8a7
witwawit <witwawit5@gmail.com>
2021-06-03 19:34:20 +0700

    init project

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(VueRouter)
+
+const routes = [
+	{ path: '/', component: HelloWorld }
+]
+
+const router = new VueRouter({
+	routes
+})
+export default router;
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    css: {
+        loaderOptions: {
+            // pass options to sass-loader
+            // @/ is an alias to src/
+            // so this assumes you have a file named `src/variables.sass`
+            // Note: this option is named as "prependData" in sass-loader v8
+            sass: {
+                prependData: `@import "~@/assets/scss/main.scss"`
+            },
+        }
+    }
+}
